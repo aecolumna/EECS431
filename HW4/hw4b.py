a459d510e25bd94867ce9781fbf624d9bfc912d3
Andres Columna <columnaa@cse.msu.edu>
2018-10-29 17:37:43 -0400

    hw4b.py

---
+++ HW4/hw4b.py
@@ -0,0 +1,76 @@
+from collections import namedtuple, Counter
+from itertools import chain, combinations
+
+from_iter = chain.from_iterable
+
+
+"""
+Find minimum covering set
+"""
+
+Candidate = namedtuple("Candidate",['identifier','skills','numSkills'])
+
+allSkillsArr = []
+
+lenMinCover = 0
+
+#input start
+n, k = [int(i) for i in input().split()]
+
+skillsSet = set(input().split())
+
+candidatesArr = [None] * n
+
+for i in range(n):
+    num = int(input())
+    arr = input().split()
+    allSkillsArr.extend(arr) # add to the tracker of all skills
+    candidatesArr[i] = set(arr)
+
+countDict = Counter(allSkillsArr)
+
+uniqueSkills = [skill for skill, num in countDict.items() if num == 1 ]
+
+newCandidatesArr = []
+deletedSkillsSet = set()
+
+# iterate through candidates
+for candidate in candidatesArr:
+    flag = False
+    for skill in uniqueSkills:
+        if skill in candidate:
+            flag = True
+            skillsSet -= candidate # remove skills possesed by dude whos definitely going
+            deletedSkillsSet |= candidate
+            lenMinCover += 1
+            break
+    if flag is False:
+        newCandidatesArr.append(candidate)
+
+candidatesArr = newCandidatesArr
+
+newCandidatesArr = [None] * len(candidatesArr)
+
+for i,candidate in enumerate(candidatesArr):
+    newSet = candidate - deletedSkillsSet
+    newCandidatesArr[i] = newSet
+
+candidatesArr = newCandidatesArr
+
+# tinker with the length you
+def Powerset(s):
+    return from_iter(combinations(s, r) for r in range(1, len(s)+1))
+
+#remove all useless skills
+
+candidatesArr = sorted(candidatesArr, reverse=True, key=lambda x: len(x))
+
+
+for subset in Powerset(candidatesArr):
+
+    s = set().union(*subset)
+
+    if s >= skillsSet:
+        lenMinCover += len(subset)
+        print(lenMinCover)
+        break
